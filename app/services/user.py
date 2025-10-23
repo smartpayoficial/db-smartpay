09f3bc010f56794211ef9f261d60a2ca8596e913
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2025-10-23 18:27:22 -0500

    FIX: Se ajusta Create USER

--- app/services/user.py
+++ app/services/user.py
@@ -4,6 +4,7 @@ from tortoise.expressions import Q
 
 from app.infra.postgres.crud.user import crud_user
 from app.infra.postgres.models.user import User
+from app.schemas.user import UserCreate
 from app.services.base import BaseService
 from uuid import UUID
 
@@ -21,6 +22,17 @@ class UserService(BaseService):
     async def get_all_with_filter(self, q_filter: Q, *, payload: Dict[str, Any] = None, skip: int = 0, limit: int = 100) -> List[User]:
         """Obtiene usuarios aplicando un filtro Q de Tortoise ORM además de los filtros regulares."""
         return await self.crud.get_all_with_filter(q_filter=q_filter, payload=payload, skip=skip, limit=limit)
+
+    async def create(self, *, obj_in: UserCreate) -> Optional[User]:
+        """Crea un usuario y precarga las relaciones para la respuesta."""
+        # Primero, crea el usuario usando el método base
+        new_user = await super().create(obj_in=obj_in)
+        
+        # Si el usuario se creó correctamente, obténlo de nuevo con las relaciones
+        if new_user:
+            return await self.get_by_id(user_id=new_user.user_id)
+        
+        return None
 
 
 user_service = UserService(crud=crud_user)
